bcbef528b81151c56bda7bbc4f50b8df480fb57c
Yamin Yassin <x210621@bcpcorp.net>
2024-08-08 19:50:02 +0100

    chore: starter packages

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -10,5 +10,5 @@ export type ThemedViewProps = ViewProps & {
 export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  return <View style={[{ backgroundColor:'pink' }, style]} {...otherProps} />;
 }
